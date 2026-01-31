9bf93227f2a00b474664212a8cc8df4f533f85ae
Anjali Singh <anjalisingh81095@gmail.com>
2026-01-31 09:43:35 +0530

    na

---
+++ operator.py
@@ -0,0 +1,68 @@
+
+# print(b/a)  decimal number output
+# print(b//a)  complete number output
+# print(b**2) power 2
+
+# area of square
+# num1 = int(input("enter a number "))
+# area = print(num1**2)
+
+# area of rectangle
+# num1 = int(input("enter a number "))
+# num2 = int(input("enter a number "))
+# area = print(num1*num2)
+
+#1 celcius to ferenhite
+# C = int(input("enter the value "))
+# F = print((C*9/5)+32)
+
+# #2 area of cicle
+# r = float(input("enter a radius "))
+# area = print(3.14*r**2)
+
+#3 area of triangle
+# b = int(input("enter the value of base "))
+# h = int(input("enter the value of height "))
+# area = print(1/2*b*h)
+
+#4 simple interest
+# P = int(input("enter a number "))
+# R = int(input("enter a number "))
+# T = int(input("enter a number "))
+# area = print((P*R*T)/100)
+
+# Area of cube
+# a = int(input("enter the value "))
+# area = print(6*a**2)
+
+# assignment operator
+# =, =+ , -=, *=, %=, /=, //=, **=
+
+# 1) swapping using 3rd variable
+# ex a =10, b = 20
+
+# a = 10
+# b = 20
+# c = a
+# a = b
+# b = c
+# print(a)
+# print(b)
+
+# 2) swapping without using 3rd variable
+# a = 10
+# b = 20
+# a = a+b
+# b = a-b
+# a = a-b
+# print(a)
+# print(b)
+
+# # 3 swapping without using 3rd variable
+# a = 10
+# b = 20
+# # a = a^b
+# # b = a^b
+# # a = a^b
+# print(a)
+# print(b)
---
+++ variable.py
@@ -0,0 +1,33 @@
+# num1 = int(input("enter a number "))
+# num2 = int(input("enter a number "))
+# print(num1+num2)
+
+# num1 = 10
+# print(id(num1))
+# print(type(num1))
+
+
+num1 = 10
+print(type(num1))
+
+num1 = 9.90
+print(type(num1))
+
+num1 = "riya"
+print(type(num1))
+
+num1 = []
+print(type(num1))
+
+num1 = ()
+print(type(num1))
+
+num1 = True
+print(type(num1))
+
+num1 = None
+print(type(num1))
+
+num1 = """ Hno 806 new market
+bhopal"""
+print(num1)
